acf98e90b0c8c8e1c78744649a51d0cf310241a1
Danish <Danish@DESKTOP-SELISVK>
2020-02-24 16:48:46 +0530

    using chrome without exe

--- Automation2020/src/test/java/newcms/newcms_base.java
+++ Automation2020/src/test/java/newcms/newcms_base.java
@@ -2,16 +2,36 @@ package newcms;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
 
 public class newcms_base {
 	
 	public WebDriver driver;
 	
+	
 	@Test
-	public void testBrowser() {
-	    WebDriver driver = new ChromeDriver();
-	    driver.manage().window().maximize();
+	public void openBrowser() {
+		
+		
+		WebDriver driver = null;
+		WebDriverManager.getInstance(CHROME).setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized"); 
+		options.addArguments("enable-automation"); 
+		options.addArguments("--no-sandbox"); 
+		options.addArguments("--disable-infobars");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-browser-side-navigation"); 
+		options.addArguments("--disable-gpu"); 
+		driver = new ChromeDriver(options); 
+		
+	    driver.get("https://www.google.com/");
 
 }
+			
 }
